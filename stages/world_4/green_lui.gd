2f0ed7a01b247ff3dd52f39bd10afca7ecaad12f
Furret78 <68061718+furret78@users.noreply.github.com>
2025-10-26 12:41:24 +0100

    4-1: Fixed Green Lui staying green even when playing as Luigi
    10/10 peak coding

--- stages/world_4/green_lui.gd
+++ stages/world_4/green_lui.gd
@@ -2,3 +2,11 @@ extends Powerup
 
 @export var luigi_spriteframes: SpriteFrames
 @onready var sprite: AnimatedSprite2D = $Sprite
+
+func _ready() -> void:
+	if CharacterManager.get_character_name() == "Luigi":
+		sprite.sprite_frames = luigi_spriteframes
+		sprite.play()
+		gravity_scale = 0.5
+		collided_wall.connect(turn_x)
+	super()
