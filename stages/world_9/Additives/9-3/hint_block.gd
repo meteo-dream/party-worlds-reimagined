ac51719dc7b5d38110e53d5b068b5326fdb28797
Furret78 <68061718+furret78@users.noreply.github.com>
2025-10-31 21:43:01 +0100

    Localization Prep 9: Template Update

--- stages/world_9/Additives/9-3/hint_block.gd
+++ stages/world_9/Additives/9-3/hint_block.gd
@@ -3,8 +3,8 @@ extends "res://engine/objects/bumping_blocks/message_block/message_block.gd"
 #@export var main_hint_list_text: Array[String]
 #@export var alternative_hint_list_text: Array[String]
 
-@export var main_hint_list: Array[String]
-@export var alternative_hint_list: Array[String]
+var main_hint_list: Array[String] = [tr("Aya will follow you around. Make her fly far!", "9-3 Boss 1 Hint 1"), tr("Her camera can reach quite far! Pit her into a corner and run like hell!", "9-3 Boss 1 Hint 2"), tr("Right when she dashes at you, run and jump as far as you can!", "9-3 Boss 1 Hint 3"), tr("Aya will dash from edge to edge. Try not to jump!", "9-3 Boss 1 Hint 4")]
+var alternative_hint_list: Array[String] = [tr("Slow and steady wins the race.", "9-3 Boss 2 Hint 1"), tr("Try to keep yourself under her as much as you can.", "9-3 Boss 2 Hint 2"), tr("Jump far away when she takes photos!", "9-3 Boss 2 Hint 3"), tr("Misdirect the homing shots that come from her photographing.", "9-3 Boss 2 Hint 4")]
 
 func _ready() -> void:
 	super()
